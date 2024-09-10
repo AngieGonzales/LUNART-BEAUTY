ae2cf0f841b8a8d66febb87aa90431fbaa2e74a4
AngieGonzales <angkgonzales@soy.sena.edu.co>
2024-09-10 15:03:25 -0500

    db

--- app/Models/usuario.py
+++ app/Models/usuario.py
@@ -4,8 +4,6 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
-db = SQLAlchemy()
-
 class Usuario(db.Model):
     __tablename__='usuario'
     id = db.Column(db.Integer, primary_key=True)
