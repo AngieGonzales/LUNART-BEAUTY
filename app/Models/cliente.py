3558dd09f9a7dd7581d7014600f5604c0d375664
AngieGonzales <angkgonzales@soy.sena.edu.co>
2024-09-10 10:46:27 -0500

    models

--- app/Models/cliente.py
+++ app/Models/cliente.py
@@ -7,16 +7,3 @@ class Cliente(db.Model, UserMixin):
     idcliente = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), nullable=False)
     passwordclien = db.Column(db.String(255), nullable=False)
-
-
-
-    
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def get_id(self):
-        return str(self.idcliente)
