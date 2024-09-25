caf25829a017219d27ea2f462edf46dcef10a486
lunfe <lunfe1408@gmail.com>
2024-09-24 22:47:36 -0500

    .

--- app/Models/carrito.py
+++ app/Models/carrito.py
@@ -2,13 +2,15 @@ from app import db
 
 class Carrito(db.Model):
     __tablename__ = 'carrito'
+    
     id = db.Column(db.Integer, primary_key=True)
     producto_id = db.Column(db.Integer, db.ForeignKey('producto.idproducto'), nullable=False)  # Clave foránea
     cantidad = db.Column(db.Integer, nullable=False)
     precio = db.Column(db.Float, nullable=False)
     subtotal = db.Column(db.Float, nullable=False)
 
-    producto = db.relationship('Producto', back_populates='carritos')
+    # Relación con Producto
+    producto = db.relationship('Producto', back_populates='carrito')  # Cambié aquí
 
     def __init__(self, producto_id, cantidad, precio):
         self.producto_id = producto_id
--- app/Models/producto.py
+++ app/Models/producto.py
@@ -2,15 +2,15 @@ from app import db
 
 
 class Producto(db.Model):
-    __tablename__='producto'
+    __tablename__ = 'producto'
+    
     idproducto = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), nullable=False)
-    precio = db.Column(db.String(255), nullable=False)
-    imagen=db.Column(db.String(255), nullable=False)
+    precio = db.Column(db.Float, nullable=False)  # Asegúrate de que sea Float
+    imagen = db.Column(db.String(255), nullable=False)
     categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.idCategoria'))
     stock = db.Column(db.Integer, default=0)
 
+    # Relaciones
     categorias = db.relationship("Categoria", back_populates="productoss")
-    carritos = db.relationship('Carrito', back_populates='producto')
-
-    
+    carrito = db.relationship('Carrito', back_populates='producto')  # Cambié aquí
