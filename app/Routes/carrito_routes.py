114730441f259e3cc47b023138f68ffadac68ee7
lunfe <lunfe1408@gmail.com>
2024-09-26 11:50:06 -0500

    .

--- app/Routes/carrito_routes.py
+++ app/Routes/carrito_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from app.Models.carrito import Carrito
+
   # Asegúrate de tener este modelo
 from app import db
 
@@ -42,27 +43,3 @@ def guardar_carrito():
             return jsonify({"message": str(e)}), 500  # Retorna el error
 
     return jsonify({"message": "Carrito guardado exitosamente"}), 201
-
-# Ruta para guardar la factura
-@bp.route('/guardar_factura', methods=['POST'])
-def guardar_factura():
-    try:
-        data = request.get_json()
-        total = data.get('total')
-        carrito = data.get('carrito')
-
-        for item in carrito:
-            nueva_factura = Factura(
-                producto_id=item.get('producto'),  # Asegúrate de que tu objeto tenga el id del producto
-                cantidad=item['cantidad'],
-                precio=item['precio'],
-                subtotal=item['cantidad'] * item['precio'],
-                total=total
-            )
-            db.session.add(nueva_factura)
-
-        db.session.commit()
-        return jsonify({'message': 'Factura guardada exitosamente!'}), 201
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'error': str(e)}), 500
